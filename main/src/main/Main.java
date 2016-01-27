6b5f0d5adff15cd4ba4b51fdda70923974a1ea2c
thomas <thomas@Thomas>
2016-01-27 12:46:19 -0500

    Testing for changes.

---
+++ main/src/main/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author thomas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("Going to test out if I can make changes.");
+    }
+    
+}
